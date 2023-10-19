c40432d5a27482ec54350df0a97317116f8c2c50
Soso-Mabulu <112823408+Soso-Mabulu@users.noreply.github.com>
2023-10-20 00:17:01 +0200

    Update 1 cookies

--- js/shoppingList.js
+++ js/shoppingList.js
@@ -3,7 +3,7 @@ function createListItem(text, price, description) {
     const listItem = document.createElement("li");
     listItem.className = "item";
     listItem.innerHTML = `
-        <input type="checkbox"> &nbsp; 
+        <input type="checkbox"> &nbsp &nbsp &nbsp; 
         <span><b>Item:</b> ${text}</span> &nbsp &nbsp &nbsp &nbsp; 
         <span><b>Price:</b> R${price.toFixed(2)}</span> &nbsp &nbsp &nbsp &nbsp; 
         <a href="#" class="show-more-link" data-toggle="modal" data-target="#itemModal">Show More</a> &nbsp; 
@@ -46,38 +46,6 @@ function updateItemList() {
         itemList.appendChild(listItem);
     }
 }
-
-
-// Function to update the items in cookies
-function updateItemsInCookies() {
-    const expirationDate = new Date();
-    expirationDate.setDate(expirationDate.getDate() + 7);
-
-    document.cookie = `groceryList=${JSON.stringify(items)}; expires=${expirationDate.toUTCString()}`;
-}
-
-// Function to update budget status
-function updateBudgetStatus() {
-    const totalBudget = parseFloat(document.getElementById("total-budget").value);
-    const totalCost = items.filter(item => item.checked).reduce((acc, item) => acc + item.price, 0);
-
-    const budgetStatus = document.getElementById("budget-status");
-    if (totalCost > totalBudget) {
-        budgetStatus.innerHTML = `You are over budget by R${(totalCost - totalBudget).toFixed(2)}`;
-        budgetStatus.style.color = "red";
-    } else {
-        budgetStatus.innerHTML = `You are under budget by R${(totalBudget - totalCost).toFixed(2)}`;
-        budgetStatus.style.color = "green";
-    }
-}
-
-// Load items from cookies (if available)
-let items = [];
-const cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)groceryList\s*=\s*([^;]*).*$)|^.*$/, "$1");
-if (cookieValue) {
-    items = JSON.parse(cookieValue);
-}
-
 // Handle budget submission
 const budgetSubmitButton = document.getElementById("budget-submit");
 budgetSubmitButton.addEventListener("click", () => {
@@ -189,6 +157,8 @@ retrieveListButton.addEventListener('click', () => {
         const reader = new FileReader();
         reader.onload = (event) => {
             items = JSON.parse(event.target.result);
+            // Update cookies with the retrieved items
+            updateItemsInCookies();
             updateItemList();
             updateBudgetStatus();
             alert('Your saved shopping list has been retrieved!');
@@ -222,3 +192,42 @@ document.querySelectorAll(".delete-button").forEach((deleteButton, index) => {
         }
     });
 });
+// Load items from cookies (if available)
+let items = [];
+const budgetInput = document.getElementById("total-budget");
+
+const cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)groceryList\s*=\s*([^;]*).*$)|^.*$/, "$1");
+if (cookieValue) {
+    items = JSON.parse(cookieValue);
+    updateItemList();
+    updateBudgetStatus();
+    
+    // Disable budget input if budget is set
+    if (items.length > 0 && !isNaN(parseFloat(budgetInput.value))) {
+        budgetInput.setAttribute("disabled", true);
+        document.getElementById("add-button").removeAttribute("disabled");
+    }
+}
+
+// Function to update the items in cookies
+function updateItemsInCookies() {
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 3); // Items expire in 3 days
+
+    document.cookie = `groceryList=${JSON.stringify(items)}; expires=${expirationDate.toUTCString()}`;
+}
+
+// Function to update budget status
+function updateBudgetStatus() {
+    const totalBudget = parseFloat(budgetInput.value);
+    const totalCost = items.filter(item => item.checked).reduce((acc, item) => acc + item.price, 0);
+
+    const budgetStatus = document.getElementById("budget-status");
+    if (totalCost > totalBudget) {
+        budgetStatus.innerHTML = `You are over budget by R${(totalCost - totalBudget).toFixed(2)}`;
+        budgetStatus.style.color = "red";
+    } else {
+        budgetStatus.innerHTML = `You are under budget by R${(totalBudget - totalCost).toFixed(2)}`;
+        budgetStatus.style.color = "green";
+    }
+}
